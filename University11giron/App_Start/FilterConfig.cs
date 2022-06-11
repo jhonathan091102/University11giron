d8a4f6ddc9671e41dc4ff242a995a53cd7d066a2
jhonathangiron <jhonathan.giron@tecsup.edu.pe>
2022-06-11 14:31:58 -0500

    Agregar archivos de proyecto.

---
+++ University11giron/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace University11giron
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
